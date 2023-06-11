484d40a2b5cab808474ecf61fd70419d8dbb679d
victor diaz <vdmpeniel@yahoo.es>
2023-06-11 17:11:38 -0400

    Added changes for al subtasks.

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int a, b, c;
+        a = 1;
+        b = 10;
+        c = 100;
+        System.out.println("" + a + b + c);
     }
 }
